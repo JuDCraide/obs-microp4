6737b00817c155101a1503dcd6b0e7a12f61f9e6
Júlia D. Craide <juliadcraide31@gmail.com>
2022-09-28 11:23:49 -0300

    Generate switch programs

--- extensions/csa/obs-examples-julia/generate_distribute_programs.py
+++ extensions/csa/obs-examples-julia/generate_distribute_programs.py
@@ -1,19 +1,19 @@
 topology = [
     {
-        "switchname": "s1", 
-        "modules": "all",
+        "switchname": "s1",         
+        "module": "all",
         "filename": "obs_main",
         "template": "../../templates/common_ethernet_template.up4",
     },
     {
         "switchname": "s2", 
-        "modules": "ipv4",
+        "module": "ipv4",
         "filename": "obs_main",
         "template": "../../templates/common_ethernet_template.up4",
     },
     {
         "switchname": "s3", 
-        "modules": "ipv6",
+        "module": "ipv6",
         "filename": "obs_main",
         "template": "../../templates/common_ethernet_template.up4",
     },
@@ -22,6 +22,17 @@ destination = "./example-julia/distribute_programs.sh"
 
 f = open(destination, "w")
 
-f.write('echo -e "\\n*********************************"')
+f.write('echo -e "\\n*********************************"\n')
+f.write('echo -e "\\n Generating switch programs with a template "\n')
+
+for switch in topology:
+    print(switch)
+    line = 'python ../generate_switch_program_w_template.py --switchname {0} --module {1} --filename {2} --template {3}\n'.format(
+        switch["switchname"],
+         switch["module"],
+         switch["filename"],
+         switch["template"],
+    )
+    f.write(line)
 
 f.close()
